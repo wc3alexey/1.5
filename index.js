813c6221e0bb7a16b994f0309846a6d0b786fd8a
wc3alexey <83168064+wc3alexey@users.noreply.github.com>
2021-08-24 11:06:04 +0300

    block-1.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let main = document.querySelector(".main");
 let button = document.querySelector('.container__button-showMore');
 button.addEventListener('click', function () {
-    main.classList.toggle("main-showMore_active");
+    
     button.classList.toggle("container__button-showMore_active");
     if (button.classList.contains("container__button-showMore_active")) {
         button.textContent = "Скрыть"
